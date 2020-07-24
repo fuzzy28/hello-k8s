e33fe8cc9f4e7eff28a02b446055d11aa784d20e
Ruelos Joel <ruelosjoel@gmail.com>
2020-07-24 19:40:21 +0800

    Added connection to db

---
+++ src/main/java/org/jrue/hellok8s/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package org.jrue.hellok8s;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
--- src/main/java/org/jrue/hellok8s/Hello.java
+++ src/main/java/org/jrue/hellok8s/Hello.java
@@ -1,18 +1,28 @@
 package org.jrue.hellok8s;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
+import java.util.List;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping
 public class Hello {
 
-    @GetMapping
+    @Autowired
+    private EmployeeRepository repository;
+
+    @GetMapping("/hello")
     public String hello() throws Exception {
         return String.format("Hello K8s! Hostname: %s", InetAddress.getLocalHost().getHostName());
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> employees() throws Exception {
+        return repository.findAll();
     }
 
 }
